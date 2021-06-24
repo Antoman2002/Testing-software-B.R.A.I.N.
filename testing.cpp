f3369e23f208ed86caa819fb0a2a0bbaaddce661
Antoman2002 <86403784+Antoman2002@users.noreply.github.com>
2021-06-24 10:20:38 +0200

    Create testing.cpp

---
+++ testing.cpp
@@ -0,0 +1,61 @@
+#include "mbed.h"
+DigitalOut trigger(D9);
+DigitalIn echo(D8);
+DigitalOut green(D4);
+DigitalOut red(D2);
+
+int correction = 0;
+Timer myTime;
+
+int distance(int correction)
+{
+    trigger = 1;
+    int distance = 0;
+
+    myTime.reset();
+    ThisThread::sleep_for(1ms);
+    trigger = 0;
+    while (echo == 0){};
+    myTime.start();
+    while (echo == 1){};
+    myTime.stop();
+    return distance = (myTime.elapsed_time().count() - correction)/58.0;
+};
+
+int mean(int samples, int correction)
+{
+    int mean = 0;
+    for (int i = 0; i < samples; i++){
+        mean+=distance(correction);
+    }
+    return mean/samples;
+}
+// main() runs in its own thread in the OS
+int main()
+{
+    myTime.reset();
+    myTime.start();
+    while(echo == 2) {};
+    green = 0;
+    red = 0;
+    correction = myTime.elapsed_time().count();
+
+    while(true){
+        int afstand = distance(correction);
+        int gemiddelde = mean(20, correction);
+        printf("distance: %d cm // mean: %d\n\r", afstand, gemiddelde);
+
+        if(gemiddelde <= 5){
+            green = 1;
+            ThisThread::sleep_for(1000ms);
+            green = 0;
+            ThisThread::sleep_for(20000ms);
+            red = 1;
+            ThisThread::sleep_for(1000ms);
+            red = 0;
+        }        
+
+        ThisThread::sleep_for(1000ms);
+    }
+
+}
